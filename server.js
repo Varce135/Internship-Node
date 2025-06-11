82628281f545077e574b83bf6da1c90e4ad4485a
Jeevan <jeevanvarghesevinu@gmail.com>
2025-06-11 14:15:05 +0530

    Value

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+app.use(cors());
+app.use(express.json());
+
+app.get('/getdata/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.send("Hello World War 3");
+});
+app.post('/createdata', (req, res) =>{
+    const data = req.body;
+    console.log(data);
+    res.send("Data Recieved");
+})
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:&{port}`);
+});
